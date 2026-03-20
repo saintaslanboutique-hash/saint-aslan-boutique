ff425c763cb1111f8f54943a4cbf181a5094d498
Azer Kazimov <azer.kazimov@yahoo.com>
2026-03-20 13:39:56 +0400

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,14 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+  const mongoose = require("mongoose");
+  require("dotenv").config();
 
+  const connectMongoDb = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {});
+      console.log('Connect to MongoDB');
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    }
+  };
 
-
-const connectMongoDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connect to MongoDB');
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-};
-
-module.exports = connectMongoDb;
+  module.exports = connectMongoDb;
